55e8f2f527f4b22c6e6beb8a7657fe00bde70d8c
mohammedchowdhury <mohammedchowdhury60@gmail.com>
2020-04-02 11:54:31 -0400

    adding dice

---
+++ JavaScriptCODEALONGRollingDice/rollingDice.js
@@ -0,0 +1,10 @@
+var numSides = prompt("How many sides does the die have?");
+
+
+function rollDice(numSides) {
+    return Math.floor(Math.random() * numSides) + 1;
+}
+
+for (var i = 0; i < 100; i++) {
+    console.log(rollDice(12));
+}
